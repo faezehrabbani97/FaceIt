e8180fa38afbfcac7d3d0758e96e550e3ad479ba
Faezeh Rabbani <101398285+faezehrabbani97@users.noreply.github.com>
2024-10-25 17:59:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
 
-# Read the requirements from the requirements.txt file
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name='FaceIt',
     version='1.0.0',
-    packages=find_packages(),
-    package_data={'':['FACEIT_codes/*']},  # Explicitly list the FaceIt package
+    packages=find_packages(),  # Automatically finds packages
+    package_data={'FaceIt': ['FACEIT_codes/*']},  # Ensure FACEIT_codes is included
     url='https://github.com/faezehrabbani97/FaceIt',
     license='MIT',
     author='Faezeh Rabbani',
     author_email='faezeh.rabbani97@gmail.com',
-    description='A pipeline for detecting and analyzing facial movements like eye-tracking and mouse muzzle detection.',
+    description='A pipeline for detecting and analyzing facial movements.',
     install_requires=requirements,
-    include_package_data=True,  # Ensure additional files are included
-    python_requires='>=3.6',
+    include_package_data=True,
 )
